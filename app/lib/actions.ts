192a55763ee0dba63ac17213ce1cbdd4566c923f
Oriol Palacios <orioloriolpalacios@outlook.com>
2024-12-07 19:25:13 -0500

    auth logic implemented

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,9 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 
 export type State = {
   errors?: {
@@ -109,4 +112,23 @@ export async function deleteInvoice(id: string) {
   }
   revalidatePath('/dashboard/invoices');
   // return { message: "Invoice deleted successfully" };
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+){
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError){
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'InvalidCredentials';
+        default:
+          return 'Something went wrong';
+      }
+    }
+    throw error;
+  }
 }
